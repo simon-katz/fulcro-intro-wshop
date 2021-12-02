197c5b6650dcc2fd98da7bfebd241dda7c3ff474
Simon Katz <sk@nomistech.com>
2021-12-02 14:56:12 +0000

    Add `nomis/grep/local-ignored-files` to .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((nomis/grep/local-ignored-files
+          .
+          ("package-lock.json"
+           "main.js"))
+         (nomis/grep/local-ignored-directories
+          .
+          ()))))
